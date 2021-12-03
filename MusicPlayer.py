31e0135573f235106e7ca21f8c8e2b76dcd3691d
David <dhaverberg@medicalalley.org>
2021-12-03 11:47:12 -0600

    changing serial port

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -12,7 +12,7 @@ class SerialMusicPlayer:
         if self.is_test:
             self.serial = self.get_test_serial()
         else:
-            self.serial = serial.Serial(port='/dev/ttyS0', baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=5)
+            self.serial = serial.Serial(port='/dev/ttyAMA0', baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=5)
 
         self.set_up()
 

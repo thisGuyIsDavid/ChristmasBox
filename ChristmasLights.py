0e7a164ff249e9bd3b84484f3fd0e369ae04553b
David <dhaverberg@medicalalley.org>
2021-12-04 18:11:41 -0600

    more debug

---
+++ ChristmasLights.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# NeoPixel library strandtest example
+# Author: Tony DiCola (tony@tonydicola.com)
+#
+# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
+# various animations on a strip of NeoPixels.
+
+import time
+from rpi_ws281x import PixelStrip, Color
+import argparse
+
+class ChristmasLights:
+    #   Tree: 0-31
+    #   Trunk: 32-39
+    #   Star: 40-46
+
+
+    def __init__(self):
+        self.strip = PixelStrip(55, 18, 800000, 10, False, 255, 0)
+        self.strip.begin()
+
+    def light_tree(self, color, wait_ms=50):
+        for i in range(0, 32):
+            self.strip.setPixelColor(i, color)
+            self.strip.show()
+            time.sleep(wait_ms / 1000.0)
+
+    def light_trunk(self, color, wait_ms=50):
+        for i in range(32, 40):
+            self.strip.setPixelColor(i, color)
+            self.strip.show()
+            time.sleep(wait_ms / 1000.0)
+
+    def light_star(self, color, wait_ms=50):
+        for i in range(40, 47):
+            self.strip.setPixelColor(i, color)
+            self.strip.show()
+            time.sleep(wait_ms / 1000.0)
+
+    def run(self):
+        self.light_tree((0, 255, 0))
+        self.light_star((255, 255, 0))
+
+if __name__ == '__main__':
+    ChristmasLights().run()

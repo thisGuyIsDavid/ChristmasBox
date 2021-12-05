4ad1eed7465d0810c47aca1998c5f7b84ae5c137
David <dhaverberg@medicalalley.org>
2021-12-05 16:12:33 -0600

    more debug

--- DFPlayer.py
+++ DFPlayer.py
@@ -8,7 +8,6 @@ class DFPlayer:
 
     def __init__(self, queue):
         self.queue = queue
-        self.is_playing = False
         self.serial = serial.Serial(port='/dev/ttyS0', baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=5)
         self.set_up()
 
